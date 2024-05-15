76663d03f9984391ab7be735fb73810595ed03cc
kibriarobin97 <kibriarobin97@gmail.com>
2024-05-16 02:03:35 +0600

    edit itemCard

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,16 +1,19 @@
 
-const MenuItem = ({item}) => {
+const MenuItem = ({ item }) => {
 
-    const {name, recipe, image, price} = item;
+    const { name, recipe, image, price } = item;
 
     return (
         <div className="flex items-center gap-4">
             <img src={image} alt="" className="w-32 h-28 rounded-tr-[200px] rounded-br-[200px] rounded-bl-[200px]" />
             <div>
-                <h3 className="uppercase text-[20px] font-medium">{name}</h3>
+                <div className="flex justify-between items-center">
+                    <h3 className="uppercase text-[20px] font-medium">{name}----------</h3>
+                    <p className="text-[#BB8506] text-[20px]">${price}</p>
+                </div>
                 <p className="text-[#737373]">{recipe}</p>
             </div>
-            <p className="text-[#BB8506] text-[20px]">${price}</p>
+            
         </div>
     );
 };
