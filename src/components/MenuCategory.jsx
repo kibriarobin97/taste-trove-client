ee79de1700a3206e32c0b45888efa98877e8017f
kibriarobin97 <kibriarobin97@gmail.com>
2024-05-17 14:05:07 +0600

    added menu page

--- src/components/MenuCategory.jsx
+++ src/components/MenuCategory.jsx
@@ -5,7 +5,7 @@ const MenuCategory = ({ items, title, img }) => {
     return (
         <div>
             {title && <Cover img={img} title={title}></Cover>}
-            <div className="grid md:grid-cols-2 gap-10 mb-10 mt-10">
+            <div className="grid md:grid-cols-2 gap-10 mb-10 mt-10 px-5 md:px-10">
                 {
                     items.map(item => <MenuItem
                         key={item._id}
@@ -13,7 +13,7 @@ const MenuCategory = ({ items, title, img }) => {
                     ></MenuItem>)
                 }
             </div>
-            <div className="mb-5 flex justify-center items-center">
+            <div className="mb-10 flex justify-center items-center">
                 <button className="btn btn-outline border-0 text-black border-b-2">ORDER YOUR FAVOURITE FOOD</button>
             </div>
         </div>
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,7 +1,10 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../components/Cover";
-import menuBg from "../assets/menu/banner3.jpg"
-import dessertImg from "../assets/menu/dessert-bg.jpeg"
+import menuBg from "../assets/menu/banner3.jpg";
+import dessertImg from "../assets/menu/dessert-bg.jpeg";
+import pizzaImg from "../assets/menu/pizza-bg.jpg";
+import soupImg from "../assets/menu/soup-bg.jpg";
+import saladImg from "../assets/menu/salad-bg.jpg";
 import useMenu from "../hooks/useMenu";
 import SectionTitle from "../components/SectionTitle";
 import MenuCategory from "../components/MenuCategory";
@@ -23,13 +26,36 @@ const Menu = () => {
             </Helmet>
             <Cover img={menuBg} title={'OUR MENU'}></Cover>
             <SectionTitle subHeading={"Don't Miss"} heading={'Todays Offer'}></SectionTitle>
+            
             {/* offered section */}
             <MenuCategory items={offered}></MenuCategory>
+
             {/* dessert section */}
             <MenuCategory
             items={dessert}
             title={'Desserts'}
             img={dessertImg}
+            ></MenuCategory>
+
+            {/* pizza section */}
+            <MenuCategory
+            items={pizza}
+            title={'Pizza'}
+            img={pizzaImg}
+            ></MenuCategory>
+
+            {/* soup section */}
+            <MenuCategory
+            items={soup}
+            title={'Soups'}
+            img={soupImg}
+            ></MenuCategory>
+
+            {/* salad section */}
+            <MenuCategory
+            items={salad}
+            title={'Salads'}
+            img={saladImg}
             ></MenuCategory>
         </div>
     );
