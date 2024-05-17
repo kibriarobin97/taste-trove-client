e5cff5a3888b9657edd5a7f4a40144133a3ccbd8
kibriarobin97 <kibriarobin97@gmail.com>
2024-05-17 13:24:06 +0600

    added dessert secion

---
+++ src/components/MenuCategory.jsx
@@ -0,0 +1,23 @@
+import Cover from "./Cover";
+import MenuItem from "./MenuItem";
+
+const MenuCategory = ({ items, title, img }) => {
+    return (
+        <div>
+            {title && <Cover img={img} title={title}></Cover>}
+            <div className="grid md:grid-cols-2 gap-10 mb-10 mt-10">
+                {
+                    items.map(item => <MenuItem
+                        key={item._id}
+                        item={item}
+                    ></MenuItem>)
+                }
+            </div>
+            <div className="mb-5 flex justify-center items-center">
+                <button className="btn btn-outline border-0 text-black border-b-2">ORDER YOUR FAVOURITE FOOD</button>
+            </div>
+        </div>
+    );
+};
+
+export default MenuCategory;
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,16 +1,36 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../components/Cover";
 import menuBg from "../assets/menu/banner3.jpg"
-import PopularMenu from "../components/PopularMenu";
+import dessertImg from "../assets/menu/dessert-bg.jpeg"
+import useMenu from "../hooks/useMenu";
+import SectionTitle from "../components/SectionTitle";
+import MenuCategory from "../components/MenuCategory";
+
 
 const Menu = () => {
+
+    const [menu] = useMenu()
+    const offered = menu.filter(item => item.category === 'offered')
+    const dessert = menu.filter(item => item.category === 'dessert')
+    const pizza = menu.filter(item => item.category === 'pizza')
+    const salad = menu.filter(item => item.category === 'salad')
+    const soup = menu.filter(item => item.category === 'soup')
+
     return (
         <div className="min-h-[calc(100vh-276px)]">
             <Helmet>
                 <title>Menu | Taste Trove</title>
             </Helmet>
             <Cover img={menuBg} title={'OUR MENU'}></Cover>
-            <PopularMenu></PopularMenu>
+            <SectionTitle subHeading={"Don't Miss"} heading={'Todays Offer'}></SectionTitle>
+            {/* offered section */}
+            <MenuCategory items={offered}></MenuCategory>
+            {/* dessert section */}
+            <MenuCategory
+            items={dessert}
+            title={'Desserts'}
+            img={dessertImg}
+            ></MenuCategory>
         </div>
     );
 };
